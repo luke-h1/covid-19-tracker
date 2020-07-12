66f39396e93325a270faa397e0737b57912e3796
luke-h1 <luke.howsam@yahoo.com>
2020-07-12 13:34:06 +0100

    api working in console.log()

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const myForm = document.getElementById('myForm') ; 
-  myForm.addEventListener('submit', function(e) { 
-  const country = document.getElementById('country').value 
-  alert(country) ; 
+myForm.addEventListener('submit', function(e) {  
+
+	e.preventDefault() ; 
+	const country = document.getElementById('country').value   
+	// fetch get request for data 
+	const url = "https://api.covid19api.com/total/dayone/country/"+country  ; 
+
+	fetch(url).then((res) => res.json()).then((res => { 
+		console.log(res) ; 
+	}))
+
+	
 })
+
