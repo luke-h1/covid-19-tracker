9f9fb068ac8a14aa1aefe808129f6d3328cae5f8
luke-h1 <luke.howsam@yahoo.com>
2020-07-14 22:56:58 +0100

    remove uneeded comments

--- app.js
+++ app.js
@@ -1,48 +1,26 @@
-const myForm = document.getElementById('myForm') ; 
-myForm.addEventListener('submit', function(e) {  
-	e.preventDefault() ;  // prevent form being submitted 
-	
-	
+const myForm = document.getElementById("myForm");
+myForm.addEventListener("submit", function (e) {
+  e.preventDefault(); // prevent form being submitted
 
+  const country = document.getElementById("country").value;
+  // fetch get request for data
+  const url = "https://api.covid19api.com/total/dayone/country/" + country;
 
-	const country = document.getElementById('country').value   
-	// fetch get request for data 
-	const url = "https://api.covid19api.com/total/dayone/country/"+country  ; 
-
-	fetch(url).then((res) => res.json()).then((res => {   
-		console.log(res) ; 
-		let length = res.length ; 
-		let index = length -1 ;  
-		let confirmed = document.getElementById('confirmed'); 
-		let recovered = document.getElementById('recovered'); 
-		let deaths = document.getElementById('deaths'); 
-		confirmed.append("Confirmed Cases:"+res[index].Confirmed)
-		recovered.append("Confirmed Recovered:"+res[index].Recovered)
-		deaths.append("Confirmed Deaths:"+res[index].Deaths)   
-
-
-
-	})); 	
-	// fix concatenation issue 
-		confirmed.innerHTML = '' 
-		recovered.innerHTML = ''  
-		deaths.innerHTML = '' 
-
-
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      let length = res.length;
+      let index = length - 1;
+      let confirmed = document.getElementById("confirmed");
+      let recovered = document.getElementById("recovered");
+      let deaths = document.getElementById("deaths");
+      confirmed.append("Confirmed Cases:" + res[index].Confirmed);
+      recovered.append("Confirmed Recovered:" + res[index].Recovered);
+      deaths.append("Confirmed Deaths:" + res[index].Deaths);
+    });
+  // fix concatenation issue
+  confirmed.innerHTML = "";
+  recovered.innerHTML = "";
+  deaths.innerHTML = "";
 });
-
-
-
-
-
-
-
-
-//       <button class="btn" id="clear" onClick="clear__Form()">Clear form</button>
-
-
-// confirmed.innerHTML = '' 
-// recovered.innerHTML = ''  
-// deaths.innerHTML = '' 
-
-// String(n).replace(/(.)(?=(\d{3})+$)/g,'$1,')
