a35b539b02f5181b57658757620ee68b71049281
luke-h1 <luke.howsam@yahoo.com>
2020-07-19 11:37:03 +0100

    fix typo

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const myForm = document.getElementById("myForm");
-myForm.addEventListener("submit", function (e) {
-  e.preventDefault(); // prevent form being submitted
+const myForm = document.getElementById('myForm');
+myForm.addEventListener('submit', function (e) {
+    e.preventDefault(); // prevent form being submitted
 
-  const country = document.getElementById("country").value;
-  // fetch get request for data
-  const url = "https://api.covid19api.com/total/dayone/country/" + country;
+    const country = document.getElementById('country').value;
+    // fetch get request for data
+    const url = 'https://api.covid19api.com/total/dayone/country/' + country;
 
-  fetch(url)
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res);
-      let length = res.length;
-      let index = length - 1;
-      let confirmed = document.getElementById("confirmed");
-      let recovered = document.getElementById("recovered");
-      let deaths = document.getElementById("deaths");
-      confirmed.append("Confirmed Cases:" + res[index].Confirmed);
-      recovered.append("Confirmed Recovered:" + res[index].Recovered);
-      deaths.append("Confirmed Deaths:" + res[index].Deaths);
-    });
-  // fix concatenation issue
-  confirmed.innerHTML = "";
-  recovered.innerHTML = "";
-  deaths.innerHTML = "";
+    fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(res);
+            const length = res.length;
+            const index = length - 1;
+            const confirmed = document.getElementById('confirmed');
+            const recovered = document.getElementById('recovered');
+            const deaths = document.getElementById('deaths');
+            confirmed.append('Confirmed Cases:' + res[index].Confirmed);
+            recovered.append('Confirmed Recovered:' + res[index].Recovered);
+            deaths.append('Confirmed Deaths:' + res[index].Deaths);
+        });
+    // fix concatenation issue
+    confirmed.innerHTML = '';
+    recovered.innerHTML = '';
+    deaths.innerHTML = '';
 });
+
