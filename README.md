016bfc0d531328f0c8be46261a367d6e0994087f
luke-h1 <luke.howsam@yahoo.com>
2020-08-06 21:14:24 +0100

    rewrite

--- README.md
+++
@@ -1,9 +0,0 @@
-# covid-19-tracker
-* Simple covid-19 tracker made with Vanilla JS, HTML &amp; CSS 
-
-# TO-DO : 
-* fix concatentation issue by clearing form after new country has been submitted  ✅
-* improve styles ✅
-
-# documentation 
- * Documentation can be found here: https://documenter.getpostman.com/view/10808728/SzS8rjbc?version=latest
--- app.js
+++ app.js
@@ -1,29 +1,63 @@
-const myForm = document.getElementById('myForm');
-myForm.addEventListener('submit', getData);  
-const country = document.getElementById('country').value;
-const API_URL = 'https://api.covid19api.com/total/dayone/country/' + country;
+const buttton = document.getElementById('button-addon1');
+const input = document.querySelector('.form-control').value;
+const API_URL = `https://corona.lmao.ninja/v2/all?${input}`;
 
+//corona.lmao.ninja/v2/jhucsse/counties/:countyName
 
+https: buttton.addEventListener('click', getResults);
 
+async function getResults() {
+    if (input === '') {
+        alert('ADD A COUNTRY!'); // turn into dynamic div element
+    } else {
+        try {
+            let start = new Date().getTime();
+            await fetch(API_URL)
+                .then((res) => res.json())
+                .then((res) => {
+                    console.log(res); 
+                    const divRow = document.createElement('div'); 
+                    divRow.className = 'row';  
+                    const colCard = document.createElement('div') 
+                    colCard.className = 'col-sm-6';  
+                    const divCard = document.createElement('div'); 
+                    divCard.className = 'card'; 
+                    const divBody = document.createElement('div'); 
+                    divBody.className = 'card-body'; 
+                    const h5 = document.createElement('h5');
+                    h5.className = 'card-title';  
+                    const paraText = document.createElement('p'); 
+                    paraText.className = 'card-text';   
 
-
-async function getData(e){
-    e.preventDefault(); 
-    try { 
-        fetch(API_URL) 
-        .then((res) => res.json()) 
-        .then((res) => { 
-            console.log(res); 
-            const length = res.length; 
-            const index = length -1 ; 
-            const confirmed = document.getElementById('confirmed'); 
-            const recovered = document.getElementById('recovered'); 
-            const deaths = document.getElementById('deaths'); 
-            confirmed.append('Confirmed Cases: ' + res[index].Confirmed); 
-            recovered.append('Confirmed Recovered: ' + res[index].Recovered); 
-            deaths.append('Confirmed Deaths: ' + res[index].Deaths);  
-        });  
-    } catch {  
-        console.log(error)
+                   
+                });
+        } catch {
+            console.log('some error');
+        }
     }
 }
+
+// API RESPONSE
+// {
+//   "updated": 1596743394219,
+//   "cases": 19139739,
+//   "todayCases": 173977,
+//   "deaths": 714235,
+//   "todayDeaths": 3948,
+//   "recovered": 12279122,
+//   "todayRecovered": 122737,
+//   "active": 6146382,
+//   "critical": 65327,
+//   "casesPerOneMillion": 2455,
+//   "deathsPerOneMillion": 91.6,
+//   "tests": 357848486,
+//   "testsPerOneMillion": 46081.23,
+//   "population": 7765601299,
+//   "oneCasePerPeople": 0,
+//   "oneDeathPerPeople": 0,
+//   "oneTestPerPeople": 0,
+//   "activePerOneMillion": 791.49,
+//   "recoveredPerOneMillion": 1581.22,
+//   "criticalPerOneMillion": 8.41,
+//   "affectedCountries": 215
+// }
